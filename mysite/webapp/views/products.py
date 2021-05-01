63bf768ae49aad1c42bef73300a50158e03d595f
asman <iasman312>
2021-05-01 12:32:05 +0600

    Создали представления для просмотра всех товаров

---
+++ mysite/webapp/views/products.py
@@ -0,0 +1,42 @@
+from django.utils.http import urlencode
+from django.views.generic import ListView
+
+from mysite.webapp.forms import SearchForm
+from mysite.webapp.models import Product
+from django.db.models import Q
+
+
+class IndexView(ListView):
+    template_name = 'products/index.html'
+    model = Product
+    context_object_name = 'products'
+    paginate_by = 5
+    paginate_orphans = 1
+
+    def get(self, request, **kwargs):
+        self.form = SearchForm(request.GET)
+        self.search_data = self.get_search_data()
+        return super(IndexView, self).get(request, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.search_data:
+            queryset = queryset.filter(
+                Q(name__icontains=self.search_data) |
+                Q(category__icontains=self.search_data)
+            )
+        return queryset
+
+    def get_search_data(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search_value']
+        return None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_form'] = self.form
+
+        if self.search_data:
+            context['query'] = urlencode({'search_value': self.search_data})
+        return context
