cd7361d95fab653e51cf6dbc6576e159bebc1440
asman <iasman312>
2021-05-01 14:27:33 +0600

    Создали представление для редактирования отзыва

--- mysite/webapp/views/feedbacks.py
+++ mysite/webapp/views/feedbacks.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from webapp.forms import FeedbackForm
 from webapp.models import Feedback, Product
@@ -19,9 +19,22 @@ class ProductFeedbackCreate(CreateView):
 
     def form_valid(self, form):
         product = get_object_or_404(Product, id=self.kwargs.get('pk'))
-
         feedback = form.instance
         feedback.product = product
         feedback.author = self.request.user
+        return super().form_valid(form)
 
+
+class ProductUpdateView(UpdateView):
+    form_class = FeedbackForm
+    model = Product
+    template_name = 'feedbacks/update.html'
+    context_object_name = 'feedback'
+
+    def get_success_url(self):
+        return reverse('webapp:view', kwargs={'pk': self.kwargs.get('pk')})
+
+    def form_valid(self, form):
+        feedback = form.instance
+        feedback.moderated = False
         return super().form_valid(form)
